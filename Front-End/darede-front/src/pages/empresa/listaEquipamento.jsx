338c000b6e307879e4e01bf0950de990bd34bc85
figueirapedro526 <figueirapedro526@gmail.com>
2022-04-06 09:53:27 -0300

    redirecuonamento

--- Front-End/darede-front/src/pages/empresa/listaEquipamento.jsx
+++ Front-End/darede-front/src/pages/empresa/listaEquipamento.jsx
@@ -13,10 +13,12 @@ import telefone from '../../img/imgCliente/telefone.png'
 import insta from '../../img/imgCliente/instagram.png'
 import linkedin from '../../img/imgCliente/linkedin.png'
 import whats from '../../img/imgCliente/whats.png'
+import chatboot from '../../img/imgCliente/icone_chat.png'
 
 
 export default class listaEquipamento extends Component {
 
+   
 
 
     render() {
@@ -44,115 +46,124 @@ export default class listaEquipamento extends Component {
 
 
                 </header>
-
-                <div className='divisaoFundo'>
-
-
-                    <div className='blocoInfo'>
-
-                        <div>
-                            <section className='posicaoI' >
-                                <img className='circuloFoto' src={Fake} alt="" />
-                            </section>
-
-                            <section className='selecaoLinhas'>
+                <div className='divisaoPrin'>
+                    <div className='divisaoFundo'>
 
 
-                                <div className='linha'></div>
+                        <div className='blocoInfo'>
 
-                                <div >
+                            <div>
+                                <section className='posicaoI' >
+                                    <img className='circuloFoto' src={Fake} alt="" />
+                                </section>
 
-                                    <div className='dividirLinha'>
-                                        <img className='iconeU' src={IconeUsuario} alt="" />
-                                        <h1>Dados Usuario</h1>
+                                <section className='selecaoLinhas'>
+
+
+                                    <div className='linha'></div>
+
+                                    <div >
+
+                                        <div className='dividirLinha'>
+                                            <img className='iconeU' src={IconeUsuario} alt="" />
+                                            <h1>Dados Usuario</h1>
+                                        </div>
+
+
+                                        <div className='dividirLinha'>
+                                            <h3 >Nome:</h3> <p>Pedro</p>
+                                        </div>
+                                        <div className='dividirLinha'>
+                                            <h3>Empresa:</h3> <p>Corinthians</p>
+                                        </div>
+                                        <div className='dividirLinha'>
+                                            <h3>Nome:</h3> <p>ti@corinthians.com</p>
+                                        </div>
+                                        <div className='dividirLinha'>
+                                            <h3>CNPJ:</h3> <p>131.213.421-89</p>
+                                        </div>
+                                        <div className='dividirLinha'>
+                                            <h3>CEL:</h3> <p>11 92134-1223</p>
+                                        </div>
+                                        <div className='dividirLinha'>
+                                            <h3>TEL:</h3> <p>11 2752-2190</p>
+                                        </div>
+
                                     </div>
+                                </section>
+
+                            </div>
 
 
-                                    <div className='dividirLinha'>
-                                        <h3 >Nome:</h3> <p>Pedro</p>
-                                    </div>
-                                    <div className='dividirLinha'>
-                                        <h3>Empresa:</h3> <p>Corinthians</p>
-                                    </div>
-                                    <div className='dividirLinha'>
-                                        <h3>Nome:</h3> <p>ti@corinthians.com</p>
-                                    </div>
-                                    <div className='dividirLinha'>
-                                        <h3>CNPJ:</h3> <p>131.213.421-89</p>
-                                    </div>
-                                    <div className='dividirLinha'>
-                                        <h3>CEL:</h3> <p>11 92134-1223</p>
-                                    </div>
-                                    <div className='dividirLinha'>
-                                        <h3>TEL:</h3> <p>11 2752-2190</p>
-                                    </div>
+
+
+                        </div>
+
+
+
+                        <div className='boneco'>
+                            <img src={boneco} alt="" />
+                        </div>
+
+                        <div className='blocoCentralPrincipal'>
+
+                            <div>
+                                <div className='divisao'>
+                                    <img src={prancheta} alt="" />
+                                    <h1>EQUIPAMENTOS</h1>
+                                </div>
+
+
+                                <div className='blocoCentral'>
+
+                                    <section >
+                                        <img src={VPC} alt="" />
+
+                                    </section>
+
+                                    <section className='informacoes'>
+                                        <h2>VPC</h2>
+                                        <p>Amazon Elastic Compute Cloud é uma parte central da plataforma de cloud computing da Amazon.com, Amazon Web Services. O EC2 permite que os usuários aluguem computadores virtuais nos quais rodam suas próprias aplicações.</p>
+                                        <center>
+                                            <button className='botaoEC2'>
+                                                <p>Mais Informções</p>
+                                            </button>
+
+                                        </center>
+                                    </section>
 
                                 </div>
-                            </section>
+
+                                <div className='blocoCentral'>
+                                    <section >
+                                        <img src={EC2} alt="" />
+                                    </section>
+
+                                    <section className='informacoes'>
+                                        <h2>EC2</h2>
+                                        <p>Amazon Elastic Compute Cloud é uma parte central da plataforma de cloud computing da Amazon.com, Amazon Web Services. O EC2 permite que os usuários aluguem computadores virtuais nos quais rodam suas próprias aplicações.</p>
+                                        <center>
+                                            <button className='botaoEC2'>
+                                                <p>Mais Informções</p>
+                                            </button>
+                                        </center>
+                                    </section>
+
+                                </div>
+
+
+
+                            </div>
+
+
+
 
                         </div>
-
-
-
-
-                    </div>
-
-
-
-                    <div className='boneco'>
-                        <img src={boneco} alt="" />
-                    </div>
-
-                    <div className='blocoCentralPrincipal'>
 
                         <div>
-                            <div className='divisao'>
-                                <img src={prancheta} alt="" />
-                                <h1>EQUIPAMENTOS</h1>
-                            </div>
-
-
-                            <div className='blocoCentral'>
-
-                                <section >
-                                    <img src={VPC} alt="" />
-
-                                </section>
-
-                                <section className='informacoes'>
-                                    <h2>VPC</h2>
-                                    <p>Amazon Elastic Compute Cloud é uma parte central da plataforma de cloud computing da Amazon.com, Amazon Web Services. O EC2 permite que os usuários aluguem computadores virtuais nos quais rodam suas próprias aplicações.</p>
-                                    <center>
-                                        <button className='botaoEC2'>
-                                            <p>Mais Informções</p>
-                                        </button>
-
-                                    </center>
-                                </section>
-
-                            </div>
-
-                            <div className='blocoCentral'>
-                                <section >
-                                    <img src={EC2} alt="" />
-                                </section>
-
-                                <section className='informacoes'>
-                                    <h2>EC2</h2>
-                                    <p>Amazon Elastic Compute Cloud é uma parte central da plataforma de cloud computing da Amazon.com, Amazon Web Services. O EC2 permite que os usuários aluguem computadores virtuais nos quais rodam suas próprias aplicações.</p>
-                                    <center>
-                                        <button className='botaoEC2'>
-                                            <p>Mais Informções</p>
-                                        </button>
-                                    </center>
-                                </section>
-
-                            </div>
-
+                            <img className='chat' src={chatboot} alt="" />
                         </div>
-
                     </div>
-
 
                 </div>
 
@@ -169,12 +180,12 @@ export default class listaEquipamento extends Component {
 
                         <div className='localizacao' >
                             <img src={localizacao} alt="" />
-                            
+
                             <p>Alameda Araguaia, 2044 - Bloco 1 - CJ 210/211
-                                    06455-000 - Alphaville, Barueri
-                                    São Paulo - Brasil
+                                06455-000 - Alphaville, Barueri
+                                São Paulo - Brasil
                             </p>
-                    
+
                         </div>
 
                         <div className='divisaoFD'>
--- Front-End/darede-front/src/pages/login/teladeLogin.jsx
+++ Front-End/darede-front/src/pages/login/teladeLogin.jsx
@@ -1,12 +1,75 @@
 import '../../css/estilo.css'
 import fundoB1 from '../../img/imgLogin/imgFundo1.png';
-import Logo from '../../img/imgLogin/darede.png'
+import Logoo from '../../img/imgLogin/darede.png'
+import axios from 'axios';
 
 import React, {Component} from 'react';
 
 export default class teladeLogin extends Component {
 
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+        email: 'pedro@corinthians.com',
+        senha: '1234',
+        erroMensagem: '',
+        isLoading: false,
+    };
+}
+
+efetuaLogin = (event) => {
+    event.preventDefault();
+    console.log('logando')
+    this.setState({ erroMensagem: '', isLoading: true });
+
+    // axios.post('http://192.168.18.9:5000/api/Login', {
+    //   email: this.state.email,
+    //   senha: this.state.senha,
+    // })
+
+    axios.post('http://192.168.18.9:5000/api/Login', {
+      email: this.state.email,
+      senha: this.state.senha,
+    })
+
+      .then((resposta) => {
+        if (resposta.status === 200) {
+
+          localStorage.setItem('usuario-login', resposta.data.token);
+          this.setState({ isLoading: false })
+
+          let base64 = localStorage.getItem('usuario-login').split('.')[1];
+
+
+          console.log(base64)
+
+        //   if (parseJwt().role === '1') {
+
+        //     this.props.history.push('/listarconsulta');
+        //     console.log('logado: ' + usuarioAutenticado());
+        //   } else if (parseJwt().role === '2') {
+        //     this.props.history.push('/listarmedicos');
+        //   } else {
+        //     this.props.history.push('/listarminhas');
+        //   }
+        }
+      })
+
+      .catch(() => {
+        this.setState({
+        
+          erroMensagem: 'E-mail ou senha inválidos, corrija NOVAMENTE',
+          isLoading: false,
+        });
+      });
+
+  }
+
+  atualizaStateCampo = (campo) => {
+    this.setState({ [campo.target.name]: campo.target.value });
+  };
+
+
 
   render(){
     return(
@@ -22,7 +85,7 @@ export default class teladeLogin extends Component {
                  <div className='bloco3'>
 
                    <div>
-                   <img class="logo" src={Logo} alt="" />
+                   <img class="logoo" src={Logoo} alt="" />
                    </div>
 
                     <div className='estilizacaoI'>
@@ -38,7 +101,12 @@ export default class teladeLogin extends Component {
                     </div>
 
                     <div>
-                      <button id='botao'>Logar</button>
+                      <button id='botao'>
+                        <a className='temporario' href="ListagemEquipamentos">
+                        Logar
+                        </a>
+                        
+                        </button>
                     </div>
 
                  </div>
